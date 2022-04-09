bf6c71825e848ef087681ffcb0054a9856b6b289
Rene Mueller <muellren@gmail.com>
2022-04-09 21:49:18 +0200

    added Ackermann function

--- src/hello/main.clj
+++ src/hello/main.clj
@@ -1,6 +1,17 @@
 (ns hello.main
-;  (:require [clojure.tools.cli :as cli])
   (:gen-class))
 
+(require '[clojure.core.match :refer [match]])
+
+(defn acker [m n]
+  (match [m n]
+   [0 n] (+ n 1)
+   [m 0] (acker (- m 1) 1)
+   [m n] (acker (- m 1) (acker m (- n 1)))))
+  
 (defn -main [& args]
-    (println "Hello, World"))
+  (let [m 4
+        n 1]
+    (println "Ackermann(" m ", " n ")")
+    (println (acker m n))))
+  
